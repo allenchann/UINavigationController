8a57ef9764d7c577d13871093a07a3d8af16dd23
allen_Chan <mac@allen.local>
2016-08-11 10:02:40 +0800

    first
    first

---
+++ UINavigationController/UINavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavigationController
+//
+//  Created by allen_Chan on 16/8/11.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UINavigationController/UINavigationControllerTests/UINavigationControllerTests.m
@@ -0,0 +1,39 @@
+//
+//  UINavigationControllerTests.m
+//  UINavigationControllerTests
+//
+//  Created by allen_Chan on 16/8/11.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UINavigationControllerTests : XCTestCase
+
+@end
+
+@implementation UINavigationControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
